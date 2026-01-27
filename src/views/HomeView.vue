67b63369153917d25467c63c1068a5336746efb2
Per Olsen <persteen22@gmail.com>
2026-01-27 12:59:41 +0100

    Adjusted thex in HomeView component for better UX

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,8 +15,8 @@ usersStore.getAll();
 <template>
     <div>
         <h1>Hi {{authUser?.username}}!</h1>
-        <p>You're logged in with Vue 3 + Pinia by HTTP Basic Authentication!</p>
-        <h3>Users from secure api endpoint:</h3>
+        <p>You're logged in with Vue 3 + Pinia by HTTP Basic Authentication</p>
+        <h3>Users served from a secure api endpoint:</h3>
         <ul v-if="users.length">
             <li v-for="user in users" :key="user.id">{{user.username}}</li>
         </ul>
